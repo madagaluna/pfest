1967e9d2d9eb218d0c16f16fe9ed1de4de379695
madagaluna <mbradley@hbs.edu>
2023-12-19 12:50:04 -0500

    move toupdated repostitory

--- script/map.js
+++ script/map.js
@@ -12,7 +12,8 @@ let porchfestData = {  // was parksData
       "description": "Guitars, mandolin, fiddle, banjo and vocals! Fun mix of Folk/Country/Bluegrass",
       "perfTime": "12:30pm",
       "perfLength": "1 hour",
-      "address": "112 Dalton Rd",
+      "SocialMedia": "Acoustic-Thursday.com",
+      "Prearranged": "112 Dalton Rd",
       "latitude": "42.3826846",
       "longitude": "-71.161134"
     },
@@ -21,7 +22,8 @@ let porchfestData = {  // was parksData
         "description": "Business Goose is a modern Jazz/Funk band based in New England.  Drawing on artists from John Coltrane to Snarky Puppy, their music is one big melting pot of styles.\nThe members met at the 2022 Litchfield Jazz Camp, and a few weeks later, they recorded their first EP, 'The Woodshop Sessions'.  Six months later, they recorded their first full length album, titled 'Bongoes with Issues', which was their first release containing original music.  Their second album was recorded at the end of July 2023, and is expected to be released within the month of September.  Stay tuned!",
         "perfTime": "2:00pm",
         "perfLength": "90 minutes",
-        "address": "116 Orchard Street",
+        "SocialMedia": "businessgoosemusic@gmail.com",
+        "Prearranged": "116 Orchard Street",
         "latitude": "42.3915439",
         "longitude": "-71.1716105"
     },
@@ -30,80 +32,83 @@ let porchfestData = {  // was parksData
         "description": "Andrew Sue Wing is a Boston based singer/songwriter and guitarist specializing in funk-rock. Equipped with a strong baritone voice and a unique style of rhythm and lead guitar, heâ€™s able to belt out soaring choruses and deliver soulful ballads. His repertoire includes both original compositions and covers of classics. With influences such as Stevie Wonder, Sam Cooke, Curtis Mayfield, and Gary Clark Jr., his originals combine blues, rock, funk, and soul.",
         "perfTime": "2:00pm",
         "perfLength": "full 2 hours",
-        "address": "156 Common St",
+        "SocialMedia": "https://andrewsuewing.com/",
+        "Prearranged": "156 Common St",
         "latitude": "42.3911322",
         "longitude": "-71.1735688"
-    }
-     /*   {
+    },
+        {
           "PerformingName": "The Berklee Professors of Belmont",
-          "Description": "Concert by The Berklee College of Music Professors who live in and around Belmont.",
+          "description": "Concert by The Berklee College of Music Professors who live in and around Belmont.",
+          "perfTime": "12:30pm",
+          "perfLength": "1 hour",
           "SocialMedia": "www.johnbaboian.com",
           "PrearrangedAddress": "122 Cross St, Belmont, MA 02478",
-          "Photo": "berklee-professors.jpg"
+          "latitude": "42.39950397755575",
+          "longitude": "-71.1689150094519"
         },
         {
           "PerformingName": "En Route Jazz Quartet",
-          "Description": "The EnRoute quartet is composed of 4 professional musicians, they perform music from Pat Metheny, John Scofield and original compositions.",
+          "description": "The EnRoute quartet is composed of 4 professional musicians, they perform music from Pat Metheny, John Scofield and original compositions.",
+          "perfTime": "4:30pm",
+          "perfLength": "1 hour",
           "SocialMedia": "jeanmariecorrois.com",
           "PrearrangedAddress": "17 Sycamore Street, Belmont, MA 02478",
-          "Photo": "en-route-jazz-quartet.jpg"
+          "latitude": "42.38677382982333",
+          "longitude": "-71.1880981211644"
         },
         {
           "PerformingName": "Box of Records",
-          "Description": "Classic rock cover band that plays your favorite tunes from the 60s through today!",
+          "description": "Classic rock cover band that plays your favorite tunes from the 60s through today!",
+          "perfTime": "2:30pm",
+          "perfLength": "2 hour",
           "SocialMedia": "https://www.facebook.com/BoxofRecordsBand?mibextid=LQQJ4d",
           "PrearrangedAddress": "6 Stella Rd, Belmont, MA 02478",
-          "Photo": "box-of-records.jpg"
+          "latitude": "42.40210851449226",
+          "longitude": "-71.17096417767691"
         },
         {
           "PerformingName": "The Motown Band",
-          "Description": "Playing the hits and pulse of Motown",
+          "description": "Playing the hits and pulse of Motown",
+          "perfTime": "1:30pm",
+          "perfLength": "2 hour",
           "SocialMedia": "https://www.youtube.com/watch?v=xBk4zltyN2c&list=PLE6NlkF0BCY0mYQYPERIyfu4q27LPwA19&pp=gAQBiAQB",
           "PrearrangedAddress": "52 Washington Street, Belmont, MA 02478",
-          "Photo": "motown-band.jpg"
+           "latitude": "42.3870083552469",
+          "longitude": "-71.17249186435949"
         },
-        {
-          "PerformingName": "Business Goose",
-          "Description": "Business Goose is a modern Jazz/Funk band based in New England. Drawing on artists from John Coltrane to Snarky Puppy, their music is one big melting pot of styles.",
-          "SocialMedia": "https://www.youtube.com/watch?v=th8dCv5BJxM",
-          "PrearrangedAddress": "116 Orchard Street, Belmont, MA 02478",
-          "Photo": "business-goose.jpg"
-        },
+
         {
           "PerformingName": "Summer Dads",
-          "Description": "Summer Dads have lit the grill and promise cover songs for everyone, from medium rare to well done. Don't be late or you'll have to ketchup!",
+          "description": "Summer Dads have lit the grill and promise cover songs for everyone, from medium rare to well done. Don't be late or you'll have to ketchup!",
+          "perfTime": "3:30pm",
+          "perfLength": "1 hour",
           "SocialMedia": "https://www.youtube.com/watch?v=HwrPr3M0JkU",
           "PrearrangedAddress": "65 Kilburn Rd, Belmont, MA 02478",
-          "Photo": "summer-dads.jpg"
+          "latitude": "42.3911322",
+          "longitude": "-71.1735688"
         },
         {
           "PerformingName": "Sarah Albright",
-          "Description": "Just a gal and her guitar and some folk music. Special fiddle guest to perform.",
+          "description": "Just a gal and her guitar and some folk music. Special fiddle guest to perform.",
+          "perfTime": "4:30pm",
+          "perfLength": "1 hour",
           "SocialMedia": "https://www.youtube.com/watch?v=ETLOVbTC1Bw&list=PLmxrtVowTqOxZrosgD6clK94dAOxt2zHy",
           "PrearrangedAddress": "24 Wiley Road, Belmont, MA 02478",
-          "Photo": "sarah-albright.jpg"
+          "latitude": "42.39142939575537",
+          "longitude": "-71.17812286500018"
         },
-        {
-          "PerformingName": "Bueller Bueller",
-          "Description": "Blasting out songs from the 80's - nothing more, nothing less",
-          "SocialMedia": "https://www.youtube.com/watch?v=9yJZZg9Shqw&list=PLE6NlkF0BCY2H3AjuTKrHQ3aJ6MULIMiG&pp=gAQBiAQB",
-          "PrearrangedAddress": "52 Washington Street, Belmont, MA 02478",
-          "Photo": "bueller-bueller.jpg"
-        },
-          {
-            "PerformingName": "Velmas Vibe",
-            "Description": "Cover band covering classic to modern hits",
-            "SocialMedia": null,
-            "PrearrangedAddress": "6 Stella Rd, Belmont, MA 02478",
-            "Photo": "velmas-vibe.jpg"
-          },
+ 
           {
             "PerformingName": "Music from Turkiye",
-            "Description": "We will perform classical and folk music from Turkey with traditional Turkish music instruments",
+            "description": "We will perform classical and folk music from Turkey with traditional Turkish music instruments",
+            "perfTime": "2:30pm",
+            "perfLength": "3 hour",
             "SocialMedia": "volkanefe.com",
             "PrearrangedAddress": "6 Gale Rd, Belmont, MA 02478",
-            "Photo": "music-from-turkiye.jpg"
-          }*/
+           "latitude": "42.388910656095966",
+           "longitude": " -71.16136700920504"
+          }
   ] 
 };
 
